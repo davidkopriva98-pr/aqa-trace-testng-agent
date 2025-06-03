ba0366e3e1d517f53800a70af29f8a01ce8a3da5
David Koprivec <david.koprivec@paurus.si>
2025-06-03 12:54:57 +0200

    removed session id from suite

--- src/main/java/com/aqanetics/agent/testng/ExecutionEntities.java
+++ src/main/java/com/aqanetics/agent/testng/ExecutionEntities.java
@@ -5,7 +5,6 @@ import com.aqanetics.agent.testng.dto.MinimalTestExecutionDto;
 public class ExecutionEntities {
 
   public static volatile Long suiteExecutionId;
-  public static volatile String suiteExecutionSessionId;
 
   public static volatile MinimalTestExecutionDto testExecution;
   public static volatile MinimalTestExecutionDto currentNotTestExecution;
