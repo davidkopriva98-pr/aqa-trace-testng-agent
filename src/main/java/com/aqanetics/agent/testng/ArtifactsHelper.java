b02a12faafdc0de546bb199d2f285c73edd7fcc9
David Koprivec <david.koprivec@paurus.si>
2025-08-08 11:11:56 +0200

    updated endpoint and error screenshot file name

--- src/main/java/com/aqanetics/agent/testng/ArtifactsHelper.java
+++ src/main/java/com/aqanetics/agent/testng/ArtifactsHelper.java
@@ -3,8 +3,8 @@ package com.aqanetics.agent.testng;
 import static com.aqanetics.agent.config.AqaConfigLoader.AGENT_API_ENDPOINT;
 import static com.aqanetics.agent.config.AqaConfigLoader.API_ENDPOINT;
 import static com.aqanetics.agent.config.AqaConfigLoader.ARTIFACTS_ENDPOINT;
+import static com.aqanetics.agent.config.AqaConfigLoader.METHOD_API_ENDPOINT;
 import static com.aqanetics.agent.config.AqaConfigLoader.SUITE_API_ENDPOINT;
-import static com.aqanetics.agent.config.AqaConfigLoader.TEST_API_ENDPOINT;
 import static com.aqanetics.agent.utils.CrudMethods.postExecutionArtifact;
 
 import com.aqanetics.agent.config.AqaConfigLoader;
@@ -62,24 +62,26 @@ public class ArtifactsHelper {
   }
 
   private static void uploadFile(
-      File artifact, Long executionId, String executionName, boolean errorScreenshot) {
-    if (ENABLED_ARTIFACTS && executionId != null) {
+      File artifact, Long methodExecutionId, String executionName, boolean errorScreenshot) {
+    if (ENABLED_ARTIFACTS && methodExecutionId != null) {
       String url =
           API_ENDPOINT
               + AGENT_API_ENDPOINT
               + SUITE_API_ENDPOINT
               + ExecutionEntities.suiteExecutionId
-              + TEST_API_ENDPOINT
-              + executionId
+              + METHOD_API_ENDPOINT
+              + methodExecutionId
               + ARTIFACTS_ENDPOINT
               + (errorScreenshot ? "/error-screenshot" : "");
-      LOGGER.debug(
-          "Uploading {} {} to test execution {}",
+      LOGGER.info(
+          "Uploading {} {} to method execution {} - {}",
           errorScreenshot ? "error screenshot" : "artifact",
           artifact.getName(),
+          methodExecutionId,
           executionName);
+      String fileName = errorScreenshot ? "error-" + artifact.getName() : artifact.getName();
       try {
-        postExecutionArtifact(url, artifact, artifact.getName(), true);
+        postExecutionArtifact(url, artifact, fileName, true);
       } catch (AqaAgentException aqaException) {
         if (aqaException.shouldThrowException()) {
           LOGGER.error("Error uploading artifact: {}", aqaException.getMessage());
